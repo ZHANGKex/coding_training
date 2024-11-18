0d0a3d9f675ffa29ab503fac80686a67751e9f59
kexin-inoco <kexin@inoco.fr>
2024-11-18 18:00:46 +0100

    try lint

--- selection_sort.py
+++ selection_sort.py
@@ -11,3 +11,5 @@ def selection_sort(arr):
 if __name__ == "__main__":
     arr = [64, 34, 25, 12, 22, 11, 90]
     print(selection_sort(arr))  # Attendu: [11, 12, 22, 25, 34, 64, 90]
+
+print("yes")
